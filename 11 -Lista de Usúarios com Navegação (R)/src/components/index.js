e0fec233f1e6f5a54d49c76e1b4ed8e877a18bb2
Wallax <amadocarilloleiva@gmail.com>
2024-01-06 01:40:31 -0300

    Atualizado 2024

--- 11 -Lista de Usúarios com Navegação (R)/src/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Item } from './Item';
-export { default as List } from './List';
-export { default as Router } from './Router';
--- 12 - Lista de Usúario Aprimorada/src/pages/index.js
+++
@@ -1,3 +0,0 @@
-export { default as DetailsPage } from './Details';
-export { default as ErrorPage } from './Error';
-export { default as HomePage } from './Home';
