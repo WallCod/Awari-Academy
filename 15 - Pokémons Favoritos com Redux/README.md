cd5368f01761aef5ee8b0661df11215f4770b19b
Wallax <amadocarilloleiva@gmail.com>
2024-07-05 01:40:52 -0300

    React-Redux

---
+++ 15 - Pokémons Favoritos com Redux/README.md
@@ -0,0 +1,5 @@
+# Prática: Pokémons Favoritos com Redux
+
+Nesta prática, a lista de Pokémons favoritos, construída na aula anterior, será incrementada com o recurso Redux.
+
+
---
+++ 15 - Pokémons Favoritos com Redux/src/store/favorites/index.js
@@ -0,0 +1,4 @@
+
+export { actions as favoritesActions } from './favorites.slice';
+export { default as favoritesSlice } from './favorites.slice';
+export { reducer as favoritesReducer } from './favorites.slice';
