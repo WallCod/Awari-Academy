44bcf9030532c2af0056c4219c3b4a6b7f5944f4
Wallax <amadocarilloleiva@gmail.com>
2024-07-14 02:31:20 -0300

    17 - Teste End-to-End / Cypress

---
+++ 17 - Teste End-To-End - Cypress/src/components/Heart/Heart.cy.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+
+import Heart from './Heart';
+
+describe('<Heart>', () => {
+    it('should render correctly', () => {
+        cy.mount(<Heart />);
+        cy.get("[data-testid='heart']").should('be.visible');
+    });
+
+    it('should render correctly when selected', () => {
+        cy.mount(<Heart selected />);
+        cy.get("[data-testid='heart']").should('be.visible');
+        cy.get("[data-testid='heart']").should('have.class', 'heart-selected');
+    });
+});
